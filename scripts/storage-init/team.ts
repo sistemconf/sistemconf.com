6373056eacef8b5919ac070e029f1e5b40b6e91d
McGovMan <c.mcgovern15@nuigalway.ie>
2022-01-15 19:36:05 +0000

    add types to import images script

--- scripts/storage-init/team.ts
+++ scripts/storage-init/team.ts
@@ -1,21 +1,16 @@
-// @ts-nocheck
-
 import { storage } from '../firebase-config';
-import { ref } from 'firebase-admin/storage';
-import data from '../../docs/default-firebase-data.json';
 const fs = require('fs');
 
 export const importImages = () => {
 
   // Get all directory names in images folder
-  const getDirectories = source =>
-    fs.readdirSync('../../images', { withFileTypes: true })
-      .filter(dirent => dirent.isDirectory())
-      .map(dirent => dirent.name);
+  const getDirectories = fs.readdirSync('../../images', { withFileTypes: true })
+    .filter((dirent: { isDirectory: () => boolean; }) => dirent.isDirectory())
+    .map((dirent: { name: string; }) => dirent.name);
 
   // Walk all directories gotten above and upload each file within
-  getDirectories().forEach(dirName => {
-    fs.readdirSync('../../images/' + dirName).forEach(file => {
+  getDirectories.forEach((dirName: string) => {
+    fs.readdirSync('../../images/' + dirName).forEach((file: string) => {
       uploadFile(dirName + '/' + file);
     })
   });
@@ -23,7 +18,7 @@ export const importImages = () => {
 
 // filename: directory + filename from ../../images onwards, e.g. teams/compsoc.jpg
 // Uploads specified file to firebase storage
-async function uploadFile(filename) {
+async function uploadFile(filename: string) {
   await storage.bucket().upload('../../images/' + filename, {
     destination: 'images/' + filename,
   });
